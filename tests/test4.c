b7a847f197653b4fa77e8d6ddafb963ce04cd22d
rwitpanda <rwit.panda@gmail.com>
2021-02-06 11:13:31 +0530

    TCs added

---
+++ tests/test4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define ll long long
+
+const int N = 1e5;
+ll a[N];
+
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	for(int i=0;i<n;i++) 
+		scanf("%d",&a[i]);
+	ll *p = a;
+	*p = *p + 100;
+	printf("%d",a[0]);
+	return 0;
+}
