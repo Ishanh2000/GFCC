eda6aa17ccd1b16dee83179d72f7e4be8e8d8fe7
gopala030 <agarwal.priyanshu.030@gmail.com>
2021-02-07 20:38:59 +0530

    Improving the test cases

--- tests/test1.c
+++ tests/test1.c
@@ -7,5 +7,14 @@ int main()
 	int hb=   22;
 	scanf("%s",a);
 	printf("String : %s\n",a);
+
+	in t n, exit_m;
+	scanf("%d",&n);
+	while(n&1==1)
+	{
+		printf("hello");
+		n ^=1;
+	}
+
 	return 0;
 }
--- tests/test2.c
+++ tests/test2.c
@@ -1,14 +1,55 @@
-#include <stdio.h>
+enum truth{tre = 0, flse = 0};
+
+/* this code is check to see the comment working */
 
 int main()
 {
-	in t n, exit_m;
-	scanf("%d",&n);
-	while(n&1==1)
-	{
-		printf("hello");
-		n ^=1;
-	}
+	int a;
+	float b;
+	char a2;
+	long _fd;
+	long 34hd;
+	short ada233;
+	static int asd;
+	unsigned float 23fd;
+
+	if(a>b);
+
+	if(a > b );
+	if(a <= b);
+	if(a << b);
+	if ( a %b);
+	if ( a % b == a2);
+	if(a-b != _fd);
+
+	a += b;
+	a++;
+
+	a + +;
+	a%=b;
+	a - = b;
+	34hd = a ;
+	23fd==5;
+
+	a = 5;
+
+	float c = 12;
+	int x = 23.4re;
+
+	int ad = b, bd = c int zx;
+
+	if(a || b);
+	if(a|b);
+	if ( a & & b);
+	if(a && b);
+
+	c = a^b;
+	c = a /b;
+	c = !b;
+
+
+
+
 
 	return 0;
 }
