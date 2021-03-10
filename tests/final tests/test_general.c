93864f7fddcff0b2d6c8eca1c2ba9f3a59a4e06f
rwitpanda <rwit.panda@gmail.com>
2021-03-10 15:56:42 +0530

    TC added

---
+++ tests/final tests/test_general.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+char func(int a, float *f)
+{
+	return a + *f;
+}
+
+int main()
+{
+	int ii;
+	for(ii=0;ii<10;ii++)
+	{
+		float e = 109.09;
+		float *r = &e;
+		if( func(8,r) == 'a' )
+		{
+			switch(ii)
+			{
+				case 2 : goto good;
+				default : printf("Hi\n");
+			}
+		}
+		else
+		{
+			const int g = 100;
+			g++;
+			while(g)
+			{
+				printf("do");
+				g--;
+			}
+		}
+		good : ;
+	}
+	return 0;
+}
