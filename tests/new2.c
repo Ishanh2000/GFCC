90634535644c41d53d04c72bb21e0a19e6fa0b63
rwitpanda <rwit.panda@gmail.com>
2021-04-08 22:57:32 +0530

    cleanup of tests

--- tests/new2.c
+++ tests/new2.c
@@ -1,124 +1,76 @@
-// int int x;
-// const const int x;
-// int newint g;
-// newint newint g;
-// const static  newint * g;
-// int ***(**x);
+int int x;
+const const int x;
+int newint g;
+newint newint g;
+const static  newint * g;
+int ***(**x);
 
-// int *const const *volatile volatile f;
+int *const const *volatile volatile f;
 
-// int **** (*x)[5][][6]; // PTR >> ARR >> BASE
-// int f()(); // PTR >> ARR >> BASE
-// int f[](); // PTR >> ARR >> BASE
-// int f(); // PTR >> ARR >> BASE
-// int f(int, newint); // PTR >> ARR >> BASE
-// int x[][];
-// int (**F)(); // PTR >> FUNC
+int **** (*x)[5][][6]; // PTR >> ARR >> BASE
+int f()(); // PTR >> ARR >> BASE
+int f[](); // PTR >> ARR >> BASE
+int f(); // PTR >> ARR >> BASE
+int f(int, newint); // PTR >> ARR >> BASE
+int x[][];
+int (**F)(); // PTR >> FUNC
 
-// int *const (* volatile f)(); // PTR >> FUNC >> PTR
-// float (*f)[4][5][6]; // PTR >> ARR
-// int *** (*(*f)(char m))[]; // PTR >> FUNC >> PTR >> ARR >> PTR
-// int *(*(*(*(*(*(*f[])[])[])[])[])[])[]; // ARR >> PTR >> ARR >> PTR >> ARR >> PTR >> ARR >> PTR >> ARR >> PTR >> ARR >> PTR >> ARR >> PTR (7)
-// int *** (*(*f)(char))(); // PTR >> FUNC >> PTR >> FUNC >> PTR
+int *const (* volatile f)(); // PTR >> FUNC >> PTR
+float (*f)[4][5][6]; // PTR >> ARR
+int *** (*(*f)(char m))[]; // PTR >> FUNC >> PTR >> ARR >> PTR
+int *(*(*(*(*(*(*f[])[])[])[])[])[])[]; // ARR >> PTR >> ARR >> PTR >> ARR >> PTR >> ARR >> PTR >> ARR >> PTR >> ARR >> PTR >> ARR >> PTR (7)
+int *** (*(*f)(char))(); // PTR >> FUNC >> PTR >> FUNC >> PTR
 
-// int f()();
-// int f[]();
-// int (f())[], (g[])();
+int f()();
+int f[]();
+int (f())[], (g[])();
 
-// void f(unsigned char a, ...); // FUNC
-// const double ff[][][]; // ARR
-// const int ** const *volatile l; // PTR
-// const unsigned int volatile static;
+void f(unsigned char a, ...); // FUNC
+const double ff[][][]; // ARR
+const int ** const *volatile l; // PTR
+const unsigned int volatile static;
 
-// int main(int);
-// int main(void);
+int main(int);
+int main(void);
 
-// #include <stdio.h>
-
-
-
-// int x (void x) {
-//         x;
-//         return 0;
-// }
-// char x;
-// int x;
-
-
-// int x (void * x) {
-//         return 0;
-// }
+#include <stdio.h>
 
 
 
-// void func(int f, int j, char **x) {
-//         f+j+k;
-// }
-// // int (*F)(char t[][45], int c);
-// int *F(char t[][45], int c);
-// // int (*F)();
-
-// //int F() {
-// //      return 9;
-// //}
-
-// int long h;
-// long const long k;
-
-// const int main(void);
-// const int main() {
-//         // goto g;
-//         const double d[10][10][10];
-//         int f();
-
-//         extern int lp;
-//         int lp;
-//         extern int lp;
+int x (void x) {
+        x;
+        return 0;
+}
+char x;
+int x;
 
 
-//         // g :
-//         func(&f, 89);
+int x (void * x) {
+        return 0;
+}
 
 
-        
-        
+
+void func(int f, int j, char **x) {
+        f+j+k;
+}
+int *F(char t[][45], int c);
+
+int long h;
+long const long k;
+
+const int main(void);
+const int main() {
+        const double d[10][10][10];
+        int f();
+        extern int lp;
+        int lp;
+        extern int lp;
+        func(&f, 89);
+        return 0;
+}
 
 
-//         return 0;
-// }
 
 
-// int x;
-;
 
-
-// // int main() {
-// //     extern int const i;
-// //     extern int i;
-// //     int x[][45];
-// // }
-
-// // int main(int ()(), int);
-// // int main(int [](), int);
-// // int main(int ()[], int);
-// // int main(int [][], int);
-// // int main(int [][78], int);
-// // int main(int **(*[])[78], void);
-// // int main(void);
-
-
-// // int main(){
-// //     char f = 's', m;
-// //     long  long long *****(***x)[](int*);
-// //     // int *const volatile* volatile volatile d()[2];
-// //     // int a;
-// //     // int a,b;
-// //     int (*f)(int, char) = func;
-// //     // {
-// //         int f = 0;
-// //     // }
-    
-// // }
-
-// // int func(...) { }
-// // int (*f)(void, void);
--- tests/relop.c
+++ tests/relop.c
@@ -5,160 +5,22 @@ int foo(int);
 void foo1();
 void foo2(int, int, ...);
 void foo3(int, int);
-// int foo(int j) {
-//   return 0;
-// }
-// void foo1() {
-// }
-// void foo2(int x, int y, ...) {
-// }
-// void foo3(int x, int y) {
-// }
+int foo(int j) {
+  return 0;
+}
+void foo1() {
+}
+void foo2(int x, int y, ...) {
+}
+void foo3(int x, int y) {
+}
 
 struct _x {
   int x; int y;
 };
 
 int main(int argc, char *argv[]) {
-  // int a, *p;
-  // float b, *pf;
-  // unsigned long long c, *d,**e;
-  // double ee[10];
-  // int arr[] = { 1, 2 };
-  // int arr1[12];
-  // a = a < p;
-  // a = a<b;
-  // p = c > d;
-  // e = e<e;
-  // d =  foo > pf;
-  // c = ee < pf;
-
-  // ee[8];
-  // ee[8][90];
-  // p[90];
-  // a[90];
-  // e[5][8][4];
-  // e['f'+9.0];
-  // ee[-4];
-  // ee[22222+21212212121212221];
-  // ee[pf];
-  // ee[a];
-  // struct _x { int x; int y; } l;
-  
-  // ee+ p;
-  // 1+foo1();
-  // ee[foo1()];
-  // +foo1();
-  // !foo1();
-  // !l;
-
-
-  // ~l;
-  // ~main;
-  // ~p;
-  // ~(ee[7]);
-  // ~b;
-  // ~arr1;
-  // ~y;
-  // ~3;
-  // ~a;
-
-  // sizeof(void);
-  // sizeof(main);
-
-  // ++main;
-  // ++foo1();
-  // ++foo();  // problematic
-  // ++arr;
-  // ++a;
-  // ++p;
-  // ++l;
-
-  // switch (arr1) {
-  // case arr1:
-  //   /* code */
-  //   break;
-  
-  // default:
-  //   break;
-  // }
-  
-  // while (foo1) ;
-
-  // !foo();
-  // foo1();
-  // foo2();
-  // foo2(45, 45, "ll");
-  // foo3();
-
-  // !foo(8);
-  // !foo1();
-  // foo2(5, 4, 56);
-  // foo2(45, 45, "ll");
-  // foo3(1, "ll");
-
-  // foo(0);
-  // {
-    // struct loc_t { int x; int y; } d;
-    // (struct loc_t *[])9;
-    // (void)9;
-    // (int)foo1;
-    
-  // }
-
-  // int ****l;
-  // *l;
-  // **l;
-  // ***l;
-  // ****l;
-  // *****l;
-  // &l;
-  // &(&l);
-
-  // int **x;
-  // int y[3][4][5];
-  // int f();
-  // **f; // problematic
-  // *x;
-  // **x;
-  // ***x;
-  // *y;
-  // **y;
-  // ***y;
-  // ****y;
-  // &(****y);
-
-  // struct { int a; int b; } p;
-  // p.g;
-  // struct _x l;
-  // struct _x c = l;
-  // // c = l;
-
-  // int *arr[-89];
-  // arr[89];
-
-  // int a = 90, b = a;
-  // int arr[] = {1, 2, 3, 4};
-  // struct _x {int x;};
-  // struct _xc *pp; // problematic
-  // struct _xc {int x; int y; };
-  // struct _xc *p;
-  // struct _xc o;
-  // p->m;
-  // p->x;
-
-  // o.x;
-  // o.l;
-
-
-
-  // int *const a;
-  // a *= 90;
-  // a=&main;
-  // a = main;
-  // a += 90;
-  // a <<= 0;
-  int a[] = {1, 2, 3,4};
+    int a[] = {1, 2, 3,4};
   int a, b = c;
 
 
@@ -166,9 +28,4 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-// struct _aaa *myFunc(struct _x*);
-
-// struct _aaa *myFunc(struct _x*c) {
-
-// }
 
